1ec9f776e70f80ada6c071cc48912420875f2663
Khaled Boussaidi <k.boussaidi@senec.com>
2024-02-04 23:34:51 +0100

    nav

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,26 @@
-export default function About() {
+import Link from 'next/link';
+import Layout from '@/app/layout';
+
+const Home = () => {
   return (
-    <>
-      About my khaled
-    </>
+    <Layout>
+      <section>
+        <Link href="/realizations">
+          Department Realizations
+        </Link>
+      </section>
+      <section>
+        <Link href="/provisions">
+          Provisions
+        </Link>
+      </section>
+      <section>
+        <Link href="/reports">
+          Reports
+        </Link>
+      </section>
+    </Layout>
   );
-}
+};
+
+export default Home;
